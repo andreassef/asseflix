596b25aa041a20a971e395ee946d67187c82ed60
andreassef <andreassef02@gmail.com>
2020-07-30 14:50:39 -0400

    Implementacao do eslinter com padrao airbnb e prop-types

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,23 +1,23 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/Logo.png';
 import './Menu.css';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
-
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="Logo Asseflix" />
-            </Link>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Logo Asseflix" />
+      </Link>
 
-            <Button as={Link} className= "ButtonLink" to="/cadastro/video">
-                Adicionar vídeo
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Adicionar vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,105 +1,97 @@
-import React, {useState} from 'react';
-import PageDefault from '../../../components/PageDefault';
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    // a ação de colocar as variaveis dentro dos colchetes significa que estamos abrindo 
-    // a função apos a atribuição 
-    const valoresIniciais = {
-        nome: '',
-        descricao: '',
-        cor: '',
-    }
-    const [categorias, setCategorias] = useState([]);
-    const [valores, setValores] = useState(valoresIniciais);
+  // a ação de colocar as variaveis dentro dos colchetes significa que estamos abrindo
+  // a função apos a atribuição
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [valores, setValores] = useState(valoresIniciais);
 
-    // função setValor, determina um valor específico para cada campo do formulario
-    function setValor(chave, valor) {
-        setValores({
-            ...valores, 
-            [chave]:valor, // a chave entre colchetes vai variar, retornando o valor de cada chave.
-        })
-    }
+  // função setValor, determina um valor específico para cada campo do formulario
+  function setValor(chave, valor) {
+    setValores({
+      ...valores,
+      [chave]: valor, // a chave entre colchetes vai variar, retornando o valor de cada chave.
+    });
+  }
 
-    // alterarEstado pega o input do atributo name do formulário
-    function alterarEstado(atributosDoEvento) {
-        //const { getAttribute, value } = atributosDoEvento.target;
-        // por meio do console.log conseguimentos visualizar as informações que a função
-        // está recebendo via parametro.
-        setValor(atributosDoEvento.target.getAttribute('name'), atributosDoEvento.target.value);   
-    }
+  // alterarEstado pega o input do atributo name do formulário
+  function alterarEstado(atributosDoEvento) {
+    // const { getAttribute, value } = atributosDoEvento.target;
+    // por meio do console.log conseguimentos visualizar as informações que a função
+    // está recebendo via parametro.
+    setValor(atributosDoEvento.target.getAttribute('name'), atributosDoEvento.target.value);
+  }
 
-    return (
-        <PageDefault>
-            <h1>cadastro de categoria: {valores.nome}</h1>
-            <form onSubmit={function handleSubmite(atributosDoEvento) {
-                atributosDoEvento.preventDefault();
-                //console.log("Você tentou enviar o form");
-                // os tres pontos antes de categorias significa que estamos abrindo tudo que tem 
-                // dentro dessa variavel e substituindo pelo segundo parametro.
-                 setCategorias([...categorias, valores])
-                 setValores(valoresIniciais)
-            }}>
-                <FormField
-                    label = "Nome da categoria"
-                    type="text"
-                    name="nome"
-                    value={valores.nome}
-                    onChange={alterarEstado}
-                />
+  return (
+    <PageDefault>
+      <h1>
+        cadastro de categoria:
+        {valores.nome}
+      </h1>
+      <form onSubmit={function handleSubmite(atributosDoEvento) {
+        atributosDoEvento.preventDefault();
+        // console.log("Você tentou enviar o form");
+        // os tres pontos antes de categorias significa que estamos abrindo tudo que tem
+        // dentro dessa variavel e substituindo pelo segundo parametro.
+        setCategorias([...categorias, valores]);
+        setValores(valoresIniciais);
+      }}
+      >
+        <FormField
+          label="Nome da categoria"
+          type="text"
+          name="nome"
+          value={valores.nome}
+          onChange={alterarEstado}
+        />
 
-                <FormField
-                    label = "Descricao"
-                    type=""
-                    name="descricao"
-                    value={valores.descricao}
-                    onChange={alterarEstado}
-                />  
+        <FormField
+          label="Descricao"
+          type="textarea"
+          name="descricao"
+          value={valores.descricao}
+          onChange={alterarEstado}
+        />
 
-                {/* <div>
-                    <label>
-                        Descrição:
-                        <textarea type="text" value={valores.descricao} onChange={alterarEstado} name="descricao"></textarea>
-                    </label>
-                </div> */}
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={valores.cor}
+          onChange={alterarEstado}
+        />
 
-                <FormField
-                    label = "Cor"
-                    type="color"
-                    name="cor"
-                    value={valores.cor}
-                    onChange={alterarEstado}
-                />
-                {/* <div>
-                    <label>
-                        Cor:
-                        <input type="color" value={valores.cor} onChange={alterarEstado} name="cor"></input>
-                    </label>
-                </div> */}
+        <div>
+          <Button>
+            Cadastro
+          </Button>
 
-                <div>
-                    <button>
-                        Cadastro
-                    </button>
-                </div>
-            </form>
+        </div>
+      </form>
 
-            <ul>
-                {categorias.map((categoria, index) => {
-                    return (
-                        <li key={`${categoria}${index}`}>
-                            {categoria.nome}
-                        </li>
-                    )
-                })}
-            </ul>
-            
-            <Link to="/">
-                Ir para Home.
-            </Link>
-        </PageDefault>
-    )
+      <ul>
+        {categorias.map((categoria, index) => (
+          <li key={`${categoria}${index}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home.
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
