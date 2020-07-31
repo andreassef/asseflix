e4fab0ff23ec2a4fcb43e05c1a3acd4a8a20c7ea
andreassef <andreassef02@gmail.com>
2020-07-31 11:23:33 -0400

    ajuste no arquivo server.js

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
-  consolse.log(`JSON Server is running in ${port}`);
+  console.log(`JSON Server is running in ${port}`);
 });
